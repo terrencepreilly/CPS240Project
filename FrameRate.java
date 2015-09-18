47daa046bfaec8dd2fca6fd3e924c15d25f983b9
Tyler Beachnau <stolenunder@yahoo.com>
2015-09-17 20:20:51 -0400

    Create FrameRate.java
    You need to have this for the FullScreenRenderingExample.java to work since it uses the FrameRate class. Create it in the package it says (javagames.util) or make a new one.

---
+++ FrameRate.java
@@ -0,0 +1,31 @@
+package javagames.util;
+
+public class FrameRate {
+	
+	private String frameRate;
+	private long lastTime;
+	private long delta;
+	private int frameCount;
+	
+	public void initialize() {
+		lastTime = System.currentTimeMillis();
+		frameRate = "FPS 0";
+	}
+	public void calculate() {
+		long current = System.currentTimeMillis(); //current time
+		delta += current - lastTime; //set delta (total number of milliseconds)
+		lastTime = current; //now current is lastTime
+		frameCount++; //increment frameCount
+				
+		if(delta > 1000) {
+			delta -= 1000; //subtract 1 second from total delta time, this will save leftover milliseconds
+			frameRate = String.format("FPS %s", frameCount);
+			frameCount = 0; //reset framerate counter
+		}
+	}
+	
+	//accessor to get frameRate value
+	public String getFrameRate(){
+		return frameRate;
+	}
+}
