3e9bd74ca281cafe42de5b40c926cf725bbb526f
Tyler Beachnau <stolenunder@yahoo.com>
2015-09-19 17:34:23 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -2,13 +2,26 @@ import javax.vecmath.Vector2f;
 import java.awt.image.BufferedImage;
 
 abstract class Character {
-	int health;
-	Vector2f location;
+	private int health;
+	
+	//a single point in 2D coordinates representing the center of our characters image
+	private Vector2f location;
+	
+	//the box collider utilizes an array of Vector2f values
+	//it will basically be 4 points. This will allow us to detect when
+	//2 characters Vector2f arrays (represented as a box) have intersected
+	//first Vector2f will represent the top 2 coordinates of the box, the second
+	//Vector2f will represent the bottom 2 coordinates.
+	//then when deteced take appropriate action (move away, take damage, etc)
+	private Vector2f [] boxcollider;
 	
 	public abstract BufferedImage getImage( /*Index for animation?*/ );
-
+	
 	public Vector2f getLocation() {
 		return location;
+	}
+	public Vector2f[] getBoxCollider() {
+		return boxCollider;
 	}
 
 	public void updateX(float x) {
