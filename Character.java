eab3fec64b7c104b15f36df00c0425dd17fdb19d
Tyler Beachnau <stolenunder@yahoo.com>
2015-09-24 15:03:10 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,35 +1,64 @@
-import javax.vecmath.Vector2f;
+package actors.util;
+
 import java.awt.image.BufferedImage;
 
-abstract class Character {
+import manipulation.util.*;
+
+/*
+ * The character is an image, and a location, it will create it's own boxcollider
+ * All you must do is pass it the image, and it's location of starting. You may set health later
+ * and there are methods for getting it's box collider and last good location. You should set the
+ * last good location anytime a collision is NOT detected. Consequently there is a method
+ * to set the location, this will in turn set the boxCollider to a new location
+ */
+
+public class Character extends Actor{
+	
+	private Vector lastGoodLocation; //last good location as determined by collisiondetection
 	private int health;
+	private BoxCollider boxCollider; //boxCollider for this Character. Should have 4 points
 	
-	//a single point in 2D coordinates representing the center of our characters image
-	private Vector2f location;
-	
-	//the box collider utilizes an array of Vector2f values
-	//it will basically be 4 points. This will allow us to detect when
-	//2 characters Vector2f arrays (represented as a box) have intersected
-	//first Vector2f will represent the top 2 coordinates of the box, the second
-	//Vector2f will represent the bottom 2 coordinates.
-	//then when deteced take appropriate action (move away, take damage, etc)
-	private Vector2f [] boxcollider;
-	
-	public abstract BufferedImage getImage( /*Index for animation?*/ );
-	
-	public Vector2f getLocation() {
-		return location;
+	//constructor
+	public Character(BufferedImage image, Vector location){
+		//character is an image, location, and a boxCollider, health set to 10 until changed
+		super(image, location);
+		lastGoodLocation = location;
+		setBoxCollider(image);
+		boxCollider.setLocation(location);
+		health = 10;
 	}
-	public Vector2f[] getBoxCollider() {
+	//accessors
+	public int getHealth(){
+		return health;
+	}
+	public Vector getLastGoodLocation(){
+		return lastGoodLocation;
+	}
+	
+	//mutators
+	public void setLastGoodLocation(Vector lastGoodLocation){
+		this.lastGoodLocation = lastGoodLocation;
+		//on a valid change of lastGoodLocation set box collider new position as well
+		boxCollider.setLocation(lastGoodLocation);
+	}
+	
+	public void setHealth(int health){
+		this.health = health;
+	}
+	public void setLocation(Vector location)
+	{
+		super.setLocation(location);
+		boxCollider.setLocation(location);
+	}
+	//implement methods from Actor
+	public BoxCollider getBoxCollider(){
 		return boxCollider;
 	}
-
-	public void updateX(float x) {
-		location.x = x;
+	protected void setBoxCollider(BufferedImage image){
+		boxCollider = new BoxCollider(image, true);
 	}
-	
-	public void updateY(float y) {
-		location.y = y;
+	//toString method
+	public String toString(){
+		return "This characters location: " + this.getLocation() + "and health: " + health + ".";
 	}
-	
 }
