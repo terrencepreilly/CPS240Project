526341cfcfac91c831af61ddf5d64b47c4d666f8
gleas1m <m.gleason84@yahoo.com>
2015-11-03 06:21:31 -0500

    Update GameClient.java

--- GameClient.java
+++ GameClient.java
@@ -1,13 +1,6 @@
 
 //Client class for our Network connection of our multi-player game
 import java.net.*;
-
-import javax.swing.JFrame;
-
-import java.applet.Applet;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.*;
 
 //
@@ -19,33 +12,38 @@ public class GameClient extends Thread{
 	int playerID;
 	int[] x = new int[10];
 	int[] y = new int[10];
-	boolean left,down,right,up;//create keys used for movement
-	int playerx;
-	int playery;
+	
 
 	//gather and assign information from/to client connecting
 	public GameClient(CollisionDetectionRenderExample game){
 		this.game = game;
-		
+
 		try{
-		System.out.println("Connecting...");//advise user that they are connecting to the server
-		socket = new Socket("localhost", 7777);//Create socket with (host, port)
-		System.out.println("Connection Successful!");
-		in = new DataInputStream(socket.getInputStream());
-		playerID = in.readInt();
-		out = new DataOutputStream(socket.getOutputStream());
-		Input input = new Input(in, this);
-		Thread thread = new Thread(input);
-		thread.start();
-		//Thread thread2 = new Thread(this);
-		//thread2.start();
-		
+
+			System.out.println("Connecting...");//advise user that they are connecting to the server
+			socket = new Socket("localhost", 7777);//Create socket with (host, port)
+			System.out.println("Connection Successful!");
+			in = new DataInputStream(socket.getInputStream());
+			playerID = in.readInt();
+			out = new DataOutputStream(socket.getOutputStream());
+			Input input = new Input(in, this);
+			Thread thread = new Thread(input);
+			thread.start();
+			Thread thread2 = new Thread(this);
+			thread2.start();
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 
 	}
-
+	//continuously update coordinates of player(s)
+	public void updateCoordinates(int pID, float x2, float y2){
+		this.x[pID] = (int) x2;
+		this.y[pID] = (int) y2;
+		System.out.println("("+pID+"): "+ x2 +", "+ y2);
+	}
+}
 class Input implements Runnable{
 	DataInputStream in;
 	GameClient client;
@@ -53,12 +51,21 @@ class Input implements Runnable{
 		this.in = in;
 		this.client = c;
 	}
-
+	
 	public void run() {
 		while(true){
-			game.run();
-
+			try{
+				int playerID = in.readInt();
+				Vector.x = in.readFloat();
+				Vector.y = in.readFloat();
+				client.updateCoordinates(playerID, Vector.x, Vector.y);
+				
+				
+		}
+		catch(IOException e){
+			e.printStackTrace();
 		}
 	}
-}
+	}
+	
 }
