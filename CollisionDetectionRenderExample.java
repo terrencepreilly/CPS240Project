5f0a1635dfc12f1ca5515a29a246f656c1ad8a22
Tyler Beachnau <stolenunder@yahoo.com>
2015-11-15 12:42:04 -0500

    UPDATE: Boundary Detection, Character Movement
    Added Boundary detection based on screen size (character bounces off edges) and implemented automatic movement, as well
    as the ability to position character based on angle input.
    
    Later will implement a mouselistener which will allow control of character via mouse motion/position on screen. For now the left and right keys simply add and subtract degrees to turn/rotate the character, and the up/down arrows increase or decrease character speed.

--- CollisionDetectionRenderExample.java
+++ CollisionDetectionRenderExample.java
@@ -1,10 +1,22 @@
+package RenderTests;
+
 import java.awt.event.*;
 import java.awt.image.*;
 import java.awt.*;
 import java.io.*;
 import java.util.*;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import manipulation.util.BoxCollider;
+import manipulation.util.Vector;
+import manipulation.util.GameKeyboard;
+import actors.util.Actor;
+import actors.util.Background;
+import actors.util.Scenic;
+import actors.util.Character;
+
 import java.awt.geom.Point2D;
 
 /**
@@ -20,6 +32,8 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 	private GraphicsDevice gd;
 	private Canvas canvas;
 	
+	private float characterSpeed = 7.0f; //the SPEED of the character
+	
 	private ArrayList<Character> allActors = new ArrayList<Character>(); //this is all the actors, which will have all their box colliders
 	private ArrayList<Scenic> allObjects = new ArrayList<Scenic>(); 
 	
@@ -28,6 +42,7 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 	//dangerous will be a test character that moves around, for collision detecting on TWO characters
 	private Character dangerous;
 	private Character mainC; //test of our drawCharacter class
+	private Background mainBackground;
 	private GameKeyboard gameKeyboard;
 
 	private boolean mainCMoved; // if mainCMoved, move Enemy //TODO reset
@@ -67,27 +82,62 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 	private void processInput(){
 		if(gameKeyboard.processInput(KeyEvent.VK_LEFT)){
 			//set the location of the character
-			mainC.setLocation(new Vector(mainC.getLocation().x - 5.0f, mainC.getLocation().y));
+			//we'll utilize an interesting method based on facing direction. Of course the keys correspond to directions.
+			//Up = 90, down = 270, left = 180, right = 0, based on euclidean plane geometry
+			//mainC.setDirection(180);
+			mainC.setDirection(mainC.getDirection() + 10);
+			//mainC.setLocation(new Vector(mainC.getLocation().x - 5.0f, mainC.getLocation().y));
 			mainCMoved = true;
 		}
 		if(gameKeyboard.processInput(KeyEvent.VK_RIGHT)){
-			mainC.setLocation(new Vector(mainC.getLocation().x + 5.0f, mainC.getLocation().y));
+			
+			mainC.setDirection(mainC.getDirection() - 10);
+			//mainC.setLocation(new Vector(mainC.getLocation().x + 5.0f, mainC.getLocation().y));
 			mainCMoved = true;
 		}
 		if(gameKeyboard.processInput(KeyEvent.VK_UP)){
-			mainC.setLocation(new Vector(mainC.getLocation().x, mainC.getLocation().y - 5.0f));
+			//mainC.setLocation(new Vector(mainC.getLocation().x, mainC.getLocation().y - 5.0f));
+			//mainC.setDirection(90);
+			characterSpeed += 0.5;
 			mainCMoved = true;
 		}
 		if(gameKeyboard.processInput(KeyEvent.VK_DOWN)){
-			mainC.setLocation(new Vector(mainC.getLocation().x, mainC.getLocation().y + 5.0f));
+			//mainC.setLocation(new Vector(mainC.getLocation().x, mainC.getLocation().y + 5.0f));
+			//mainC.setDirection(270);
+			characterSpeed -= 0.5;
+			//now we've set the direction. The player is ALWAYS moving, so we simply change the location EVERY update
 			mainCMoved = true;
-		}
+		}		
+		//this is the current x,y coord of our character
+		float xCoord = mainC.getLocation().x, yCoord = mainC.getLocation().y;
+		//this is our characters x, y coord translated to a euclidean plane, with the monitors center as point (0,0)
+		float properXCoord, properYCoord;
+		properXCoord = xCoord - (canvas.getWidth() / 2);
+		properYCoord = (canvas.getHeight() / 2) - yCoord;		
+		
+		//THIS represents our position vector based on our speed. You see we're TRYING to
+		//go a certain distance with this position vector, but we need to determine it's x and y coord.
+		//we ALWAYS know the length of this vector, it's the characterSpeed!!!
+		float directionX, directionY;
+		directionX = (float) (Math.cos(Math.toRadians(mainC.getDirection())) * characterSpeed); //return position vectors X coordinate
+		directionY = (float) (Math.sin(Math.toRadians(mainC.getDirection())) * characterSpeed); //return position vectors Y coordinate
+		
+		properXCoord += directionX; //add characters x position with position vector
+		properYCoord += directionY; // "           " y "                           "
+				
+		//now change coordinates BACK to monitor coordinates
+		xCoord = properXCoord + (canvas.getWidth() / 2);
+		yCoord = (canvas.getHeight() / 2) - properYCoord;
+		//now that we have the position we know what coords to add.
+		//simply add these to the character current x and y coord (in standard form
+		mainC.setLocation(new Vector(xCoord, yCoord)); //simply update characters location.
 	}
 
 	/**
 	 * Move the enemy.  Enemy determines whether to use simpleStep or AStar.
 	 */
 	private void simpleMoveEnemy() {
+		/*
 		dangerous.buildPath(
 			mainC.getLocation(),
 			displayMode.getWidth(),
@@ -97,6 +147,7 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 			mainC.getBoxCollider()
 		);
 		dangerous.step();
+		*/
 	}
 
 	// This function is FUGLY.  
@@ -159,27 +210,68 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 			if(!needToReset){
 				c.setLastGoodLocation(c.getLocation());
 			}
-		}		
+		}
+		/**
+		 * Code below is horribly ugly, sorry (Tyler), but it works well enough for keeping the character within bounds
+		 * and correctly enough bounces the character off at angles. Tried finding different methods for calculating
+		 * angles but this one seemed the simplest. if anyone has a better method, please feel free to change it!
+		 */
+		//THIS CODE performs background/boundary checking on the characters and repositions and bounces character off "walls"
+		if(mainC.getLocation().x > canvas.getWidth() || mainC.getLocation().x < 0
+				|| mainC.getLocation().y < 0 || mainC.getLocation().y > canvas.getHeight()){			
+			float x = mainC.getLocation().x, y = mainC.getLocation().y; //grab current character coordinates
+			int angle = mainC.getDirection(); //grab characters angle of direction
+			if(x < 0){ //hitting left wall
+				if(angle >= 180){
+					angle = 360 - angle % 90; //change angle for bounce back
+				} else{
+					angle = 90 - angle % 90;
+				}
+				mainC.setLocation(new Vector(0, y));
+			} else if( y < 0){ //hitting top wall
+				if(angle >= 90){
+					angle = 270 - angle % 90;
+				} else{
+					angle = 360 - angle % 90;
+				}
+				mainC.setLocation(new Vector(x, 0));
+			} else if( x > canvas.getWidth()){ //hitting right wall
+				if(angle >= 270){
+					angle = 270 - angle % 90;
+				} else{
+					angle = 180 - angle % 90;
+				}
+				mainC.setLocation(new Vector(canvas.getWidth(), y));
+			} else{ //hit bottom
+				if(angle >= 270){
+					angle = 90 - angle % 90;
+				} else{
+					angle = 180 - angle % 90;
+				}
+				mainC.setLocation(new Vector(x, canvas.getHeight()));
+			}
+			mainC.setDirection(angle); //set new direction
+		}
 	}
 	
 	/**
-	 * Render the frame. ???
+	 * Render the frame using double buffering strategy, double buffer, two do loops :)
 	 */
 	private void renderFrame() {
 		do{
 			do{
-				Graphics g = null;
+				Graphics g = null; //create Graphics object
 				try{
-					g = bs.getDrawGraphics();
-					g.clearRect(0, 0, displayMode.getWidth(), displayMode.getHeight());
-					render(g);
+					g = bs.getDrawGraphics(); //get graphics from our bufferstrategy
+					g.clearRect(0, 0, displayMode.getWidth(), displayMode.getHeight()); //clear the screen (basically draws rectangle over the whole screen
+					render(g); //call OUR custome render function every time
 				} finally {
-					if(g != null){
+					if(g != null){ //dispose if g is not null
 						g.dispose();
 					}
 				}
 			}while(bs.contentsRestored());
-			bs.show();
+			bs.show(); //make next available buffer visible
 		} while(bs.contentsLost());
 	}
 	
@@ -230,20 +322,20 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 		BufferedImage mainPlayerImage = null;
 		BufferedImage mainObstacleImage = null;
 		try {
-			mainPlayerImage = ImageIO.read(new File("character.png"));
-			mainObstacleImage = ImageIO.read(new File("obstacle.png"));
+			mainPlayerImage = ImageIO.read(new File("/Users/TheOne/Pictures/personal/mainCharacterIdle_1.png"));
+			mainObstacleImage = ImageIO.read(new File("/Users/TheOne/Pictures/personal/mainObstacle.png"));
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		dangerous = new Character(mainPlayerImage, new Vector(800, 200));
+		dangerous = new Character(mainPlayerImage, new Vector(canvas.getWidth() / 4, canvas.getHeight() / 4));
 		//set image and location of our drawCharacter class
-		mainC = new Character(mainPlayerImage, new Vector(600, 400));
-		
-
+		mainC = new Character(mainPlayerImage, new Vector(canvas.getWidth() / 2, canvas.getHeight() / 2));
+		mainBackground = new Background(canvas.getWidth(), canvas.getHeight());
+		/*
 		for (Vector v : TEMPobsplace.getObstacles()) 
 			allObjects.add( new Scenic(mainObstacleImage, v ));
-		
+		*/
 		//add characters to the actor arraylist
 		allActors.add(mainC);
 		allActors.add(dangerous);
@@ -263,6 +355,9 @@ public class CollisionDetectionRenderExample extends JFrame implements Runnable 
 		processInput();
 		detectCollisions();
 		renderFrame();
+		//uncommenting the line below increases players speed over time, DO NOT use a value at or greater than 1, values lower than .08 are pretty acceptable
+		//but player speed can quickly go out of control, once we have a boundary it'll be nicer, and we can set a speed cap as well
+		//characterSpeed += 0.08f;
 		if (mainCMoved) 
 			simpleMoveEnemy();
 		sleep(10L);
