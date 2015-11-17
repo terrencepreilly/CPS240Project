c0c8bc04a0a86972796667c544896556aec60d6f
Terrence Reilly <terrencepreilly@gmail.com>
2015-11-17 17:45:19 -0500

    Debugging Enemy client

--- ZombieClient.java
+++ ZombieClient.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A class which handles the enemy characters of the game.  Should be
@@ -9,6 +10,7 @@ public class ZombieClient extends AbstractClient implements Runnable {
 	private Thread zombieThread;
 	private boolean running;  // goes forever (will be contained in Server)
 				// Is this a good idea?
+	private HashMap<Character, Integer> targetMap; // each zombie's target
 
 	/**
 	 * Create a new instance of ZombieClient, which holds an array of Zombies
@@ -29,12 +31,53 @@ public class ZombieClient extends AbstractClient implements Runnable {
 	}
 
 	/**
+	 * For each zombie, assign a target (some character in the array after
+	 * the last zombie was added.)  Will choose whichever actor that is not
+	 * an ENEMY, which is closest to this zombie.
+	 */
+	private void assignTargets() {
+		for (Character zomb : zombies) {
+			Vector zv = zomb.getBoxCollider().getLocation();
+			Integer closeP = -1;
+
+			for (Integer cuid : gamestate.characters.keySet()) {
+				Character cplayer = gamestate.characters.get(cuid);
+				if (cplayer.getType() == ENEMY)
+					continue;
+
+				Vector pv = cplayer.getBoxCollider().getLocation();
+				if (closeP == -1)
+					closeP = cuid;
+
+				Vector cpv = gamestate.characters.get(closeP).getBoxCollider().getLocation(); 
+
+				if (zv.distance(pv) < zv.distance(cpv))
+					closeP = cuid;
+			}
+
+			targetMap.put(zomb, closeP);
+		}
+	}
+
+	private void moveCharacter(Character z) {
+		BoxCollider targetBC = gamestate.characters.get( targetMap.get(z) ).getBoxCollider();
+		z.buildPath(
+			targetBC.getLocation(), // target
+			1000, // DisplayMode???????????
+			800,
+			gamestate.characters, // TODO MUST BE ARRAYLIST
+			gamestate.obstacles, // TODO MUST BE ARRAYLIST
+			targetBC// target
+		);
+		z.step();
+	}
+
+	/**
 	 * Update positions of all enemies, send to Server.
 	 */
 	private void moveCharacters() { 
-		System.out.println("Move Characters!");
 		for (Character z : zombies)
-			System.out.println("\t" + z);
+			moveCharacter(z);
 	}
 
 	/**
