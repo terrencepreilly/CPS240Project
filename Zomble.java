ead8b81271afb035fe3feb07c196742efa5a0b04
gleas1m <m.gleason84@yahoo.com>
2015-11-21 22:00:23 -0500

    added code to Zomble class for character movement

--- Zomble.java
+++ Zomble.java
@@ -4,7 +4,8 @@ import java.awt.DisplayMode;
 import java.awt.Graphics;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-
+import java.awt.Image;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -31,18 +32,20 @@ import javax.swing.SwingUtilities;
 public class Zomble extends JFrame implements Runnable, GameConstants {
 	private GameState gamestate;		// Game & Charaters
 	private Character player;
-
 	private Client client;			// Networking
-
+	private int frames = 0;		//frame count for character movement
 	private BufferStrategy bs;		// Graphics
 	private DisplayMode displayMode;
 	private Canvas canvas;
 	private BufferedImage playerImage;
 	private BufferedImage obstacleImage;
 	private BufferedImage enemyImage;
-	
+	private BufferedImage charImage;//south facing
+	private BufferedImage charImage2;//east facing
+	private BufferedImage charImage3;//west facing
+	private BufferedImage charImage4;//north facing
 	private GameKeyboard gameKeyboard;	// UI
-
+	private Image background;
 	private volatile boolean running;	// Game Logic
 	private Thread gameThread;
 
@@ -86,8 +89,6 @@ public class Zomble extends JFrame implements Runnable, GameConstants {
 	 */
 	private void processInput() {
 		gameKeyboard.processInput();
-		client.writeGameDelta();
-		client.requestUpdate();
 	}
 
 	/**
@@ -116,6 +117,7 @@ public class Zomble extends JFrame implements Runnable, GameConstants {
 	 * @param g The graphics context.
 	 */
 	private void render(Graphics g) {
+		player.setImage(action(playerImage));
 		HashMap<Integer, Character> characters = gamestate.getCharacters();
 		for (Integer uid : characters.keySet()) {
 			Character c = characters.get(uid);
@@ -135,6 +137,95 @@ public class Zomble extends JFrame implements Runnable, GameConstants {
 			);
 		}
 	}
+	/* 
+	 *Make background of character transparent 
+	 */
+	public BufferedImage makeTransparent(BufferedImage image){
+		BufferedImage transparent = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		for(int i = 0; i<image.getWidth(); i++){
+			for(int j = 0; j<image.getHeight(); j++){
+				if(image.getRGB(i, j) != image.getRGB(0, 0)){
+					transparent.setRGB(i, j, image.getRGB(i, j));
+				}
+				
+			}
+		}
+		return transparent;
+	}
+	/*
+	 * Returns the correct image based on character direction
+	 */
+	public BufferedImage action(BufferedImage move){
+		try {
+			charImage = ImageIO.read(new File("playerFrwd.png"));
+			charImage = makeTransparent(charImage);
+			charImage2 = ImageIO.read(new File("img2.png"));
+			charImage2 = makeTransparent(charImage2);
+			charImage3 = ImageIO.read(new File("img3.png"));
+			charImage3 = makeTransparent(charImage3);
+			charImage4 = ImageIO.read(new File("img4.png"));
+			charImage4 = makeTransparent(charImage4);
+			//mainPlayerImage = ImageIO.read(new File("character.png"));
+			//mainObstacleImage = ImageIO.read(new File("obstacle.png"));
+			frames++;
+			if(frames >= 50){
+				frames = 0;
+			}
+			if(gameKeyboard.charDirection == 1){
+				playerImage = charImage;
+			}
+			if(gameKeyboard.charDirection == 2){
+				if(gameKeyboard.keyPressed[KeyEvent.VK_RIGHT]){
+					if(frames > 0 && frames <25){
+						playerImage = charImage2;
+					}
+					else if(frames >=25 && frames < 50){
+						playerImage = charImage3;
+					}
+				}else{
+					playerImage = charImage2;
+				}
+			}
+			if(gameKeyboard.charDirection == 3){
+				if(gameKeyboard.keyPressed[KeyEvent.VK_LEFT]){
+					if(frames >0 && frames <25){
+						playerImage = horizontalFlip(charImage2);
+					}
+					else if(frames >=25 && frames < 50){
+						playerImage = horizontalFlip(charImage3);
+					}
+				}else{
+					playerImage = horizontalFlip(charImage2);
+				}
+			}
+			if(gameKeyboard.charDirection == 0){
+				playerImage = charImage4;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			/*frames++;
+			if(frames>=30){
+				frames = 0;
+			}*/
+			
+		
+			return move;
+	}
+	/*
+	 * flips character image so they are facing correct way when going west
+	 */
+	public static BufferedImage horizontalFlip(BufferedImage img) {
+        int width = img.getWidth();
+        int height = img.getHeight();
+        BufferedImage flippedImage = new BufferedImage(width, height, img.getType());
+        Graphics g = flippedImage.createGraphics();
+        g.drawImage(img, 0, 0, width, height, width, 0, 0, height, null);
+        g.dispose();
+        return flippedImage;
+    }
+
 
 	/**
 	 * Initialize global variables, load images, set obstacles.
@@ -146,9 +237,9 @@ public class Zomble extends JFrame implements Runnable, GameConstants {
 		enemyImage = null;
 
 		try {
-			playerImage = ImageIO.read(new File("character.png"));
+			playerImage = ImageIO.read(new File("playerFrwd.png"));
 			obstacleImage = ImageIO.read(new File("obstacle.png"));
-			enemyImage = ImageIO.read(new File("character.png"));
+			enemyImage = ImageIO.read(new File("zombieFrwd.png"));
 
 		} catch (IOException e) {
 			e.printStackTrace();
